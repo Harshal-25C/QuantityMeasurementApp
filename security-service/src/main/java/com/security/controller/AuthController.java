18aed189f26caeb8da2b4b644b0d923c2901ec65
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-04-17 00:38:00 +0530

    [Harshal-25C] : Update AuthController to handle CORS Erros

--- security-service/src/main/java/com/security/controller/AuthController.java
+++ security-service/src/main/java/com/security/controller/AuthController.java
@@ -16,7 +16,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
